45bd3391b3893f4fa60f07316c9a40c3f34f01dd
marikomkheidze <110020576+marikomkheidze@users.noreply.github.com>
2022-10-19 13:44:48 +0400

    final

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let error = document.getElementById("error")
-
-function validateName() {
-    let name = document.getElementById("username").value;
-    if (name.length < 1) {
+function validateName(nameid, errorid, n) {
+    let error = document.getElementById(errorid)
+    let name = document.getElementById(nameid).value;
+    if (name.length < n) {
         console.log(name)
         error.classList.remove('hide')
     }
     else
         error.classList.add('hide')
 }
-
 document.getElementById("form").addEventListener("click", function (event) {
     event.preventDefault()
-    validateName();
+    validateName(nameid = 'username', errorid = 'username_error', n = 1);
+    validateName(nameid = 'usersurname', errorid = 'usersurname_error', n = 1);
+    validateName(nameid = "email", errorid = "email_error", n = 10);
+    validateName(nameid = "textarea", errorid = "message_error", n = 30);
 });
